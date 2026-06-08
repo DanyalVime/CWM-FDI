6e81cd9de836ec704315a02f5342a3d3a1846f68
Danyal <danyalikhan.dk@gmail.com>
2026-06-08 16:46:34 +0100

    change 3

--- scripts/plot.py
+++ scripts/plot.py
@@ -4,11 +4,11 @@ import matplotlib.pyplot as plt
 
 
 # parameters to modify 
-filename="my_input.txt"
+filename="time_c.txt"
 label='label'
-xlabel = 'xlabel'
-ylabel = 'ylabel'
-title='Simple plot'
+xlabel = 'Measurement number'
+ylabel = 'Cumulative probability'
+title=''
 fig_name='my_graph.png'
 bins=100 #adjust the number of bins to your plot
 
@@ -19,16 +19,17 @@ t = np.loadtxt(filename, delimiter=" ", dtype="float")
 #plt.plot(t[:,0], t[:,1], label=label)  # Plot some data on the (implicit) axes.
 
 ## if your data is "X" (1 col), use the following line
-plt.plot(t, label=label)  # Plot some data on the (implicit) axes.
+#plt.plot(t, label=label)  # Plot some data on the (implicit) axes.
 
 ## comment the lines above and uncomment the line below to plot a simple CDF
 #plt.hist(t[:], bins, density=True, histtype='step', cumulative=True, label=label)
 
 ## comment the lines above and uncomment the 4 lines below for a nicer CDF
-#n = np.arange(1,len(t)+1) / float(len(t))
-#ts = np.sort(t)
-#fig, ax = plt.subplots()
-#ax.step(ts,n)
+n = np.arange(1,len(t)+1) / float(len(t))
+ts = np.sort(t)
+fig, ax = plt.subplots()
+ax.step(ts,n)
+ax.set_xscale('log') #remove
 
 plt.xlabel(xlabel)
 plt.ylabel(ylabel)
